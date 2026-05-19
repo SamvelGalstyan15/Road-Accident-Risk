c05e814084af372ca789a30cb12f016e1a197df2
SamvelGalstyan15 <sgalstyan1511@gmail.com>
2026-05-19 15:02:25 +0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,7 +5,12 @@ import joblib
 def predict(test_data):
   try:
     model = joblib.load('road_accident_risk_model.pkl')
-    pred = model.predict(test_data)
+
+    if ',' in test_data:
+      input = [[float(i) for i in test_data.split(',')]]
+    else:
+      input = [[float(test_data)]]
+    pred = model.predict(input)
     return pred[0]    
   except Exception as ex:
     print(type(ex).__name__)
@@ -13,5 +18,5 @@ def predict(test_data):
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
-    predict(sys.argv[1])
+    print(predict(sys.argv[1]))
 
